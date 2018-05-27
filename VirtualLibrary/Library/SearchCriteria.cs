e73d111f52f4f4de7b8f63fd6761ee3c6f519f1b
Aeris94 <ada.wronowska@gmail.com>
2018-05-27 18:27:42 +0200

    Add files via upload

---
+++ VirtualLibrary/Library/SearchCriteria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibrary.Library
+{
+    public class SearchCriteria
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public string Isbn { get; }
+        public int? Weeks { get; }
+        public bool? Borrowed { get; }
+
+        public SearchCriteria(
+            string title = null,
+            string author = null,
+            string isbn = null,
+            int? weeks = null,
+            bool? borrowed = null
+            )
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Isbn = isbn;
+            this.Weeks = weeks;
+            this.Borrowed = borrowed;
+        }
+    }
+}
